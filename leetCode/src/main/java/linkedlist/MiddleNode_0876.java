0831540750df43eeb872d78d3175a1e0be026450
tallon <352420160@qq.com>
2020-03-23 22:36:10 +0800

    常规开发

---
+++ leetCode/src/main/java/linkedlist/MiddleNode_0876.java
@@ -0,0 +1,30 @@
+package linkedlist;
+
+/**
+ * 链表的中间结点
+ * https://leetcode-cn.com/problems/middle-of-the-linked-list/
+ *
+ * @author tallon
+ * @version v1.0.0
+ * @date 2020-03-23 12:19
+ */
+public class MiddleNode_0876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode p = head;
+        ListNode q = head;
+        while (q !=null && q.next != null){
+            q = q.next.next;
+            p = p.next;
+        }
+        return p;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
