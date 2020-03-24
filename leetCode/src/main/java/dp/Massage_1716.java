f6e6326eebde26fab1697f93e583a05c02750428
tallon <352420160@qq.com>
2020-03-24 11:17:46 +0800

    常规开发

--- leetCode/src/main/java/dp/Massage_1716.java
+++ leetCode/src/main/java/dp/Massage_1716.java
@@ -1,15 +1,21 @@
 package dp;
 
 /**
- * https://leetcode-cn.com/problems/house-robber/
+ * 按摩师
+ * https://leetcode-cn.com/problems/the-masseuse-lcci/
+ *
+ * @author tallon
+ * @version v1.0.0
+ * @date 2020-03-24 10:56
  */
-public class Rob_0198 {
+public class Massage_1716 {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,1};
-        System.out.println(rob(nums));
+        int[] arr = {1, 2, 3, 1};
+        System.out.println(massage(arr));
+
     }
 
-    public static int rob(int[] nums) {
+    public static int massage(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         } else if (nums.length == 1) {
@@ -23,7 +29,6 @@ public class Rob_0198 {
         for (int i = 2; i < nums.length; i++) {
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
-        return dp[nums.length-1];
+        return dp[nums.length - 1];
     }
 }
-
