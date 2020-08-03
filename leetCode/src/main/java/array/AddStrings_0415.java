cc1d371ed2b7e938c073d8c1d4f1f3807e2c4954
tallon <talloner@qq.com>
2020-08-03 10:21:17 +0800

    每日一题

---
+++ leetCode/src/main/java/array/AddStrings_0415.java
@@ -0,0 +1,27 @@
+package array;
+
+/**
+ * 字符串模拟求和
+ *
+ * @author tallon
+ * @version v1.0.0
+ * @date 2020-08-03 10:00
+ */
+public class AddStrings_0415 {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+        StringBuilder ans = new StringBuilder();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+        }
+        // 计算完以后的答案需要翻转过来
+        ans.reverse();
+        return ans.toString();
+    }
+}
