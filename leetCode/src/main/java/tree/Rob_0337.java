5640dc7e1769b487ba64d229980627c5f0462df4
tallon <talloner@qq.com>
2020-08-05 12:01:02 +0800

    每日一题

---
+++ leetCode/src/main/java/tree/Rob_0337.java
@@ -0,0 +1,31 @@
+package tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 打家劫舍
+ *
+ * @author tallon
+ * @version v1.0.0
+ * @date 2020-08-05 11:39
+ */
+public class Rob_0337 {
+    Map<TreeNode, Integer> f = new HashMap<>();
+    Map<TreeNode, Integer> g = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        dfs(root);
+        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+    }
+
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.left);
+        dfs(node.right);
+        f.put(node, node.val + g.getOrDefault(node.left, 0) + g.getOrDefault(node.right, 0));
+        g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
+    }
+}
