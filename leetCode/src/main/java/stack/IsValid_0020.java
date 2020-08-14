e7c97d5c160e26094a50875d510a3763eae16c66
tallon <352420160@qq.com>
2020-08-14 10:26:43 +0800

    每日一题

---
+++ leetCode/src/main/java/stack/IsValid_0020.java
@@ -0,0 +1,41 @@
+package stack;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 有效的括号
+ *
+ * @author tallon
+ * @version v1.0.0
+ * @date 2020-08-14 10:22
+ */
+public class IsValid_0020 {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
