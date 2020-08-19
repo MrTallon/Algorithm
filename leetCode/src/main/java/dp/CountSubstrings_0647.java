5aecc2f4301a39787929957cfa523c12979c1515
tallon <352420160@qq.com>
2020-08-19 11:42:27 +0800

    每日一题(马拉车)

---
+++ leetCode/src/main/java/dp/CountSubstrings_0647.java
@@ -0,0 +1,59 @@
+package dp;
+
+/**
+ * 回文子串
+ * https://leetcode-cn.com/problems/palindromic-substrings/solution/hui-wen-zi-chuan-by-leetcode-solution/
+ *
+ * @author tallon
+ * @version v1.0.0
+ * @date 2020-08-19 11:00
+ */
+public class CountSubstrings_0647 {
+
+    public int countSubstrings(String s) {
+        int n = s.length(), ans = 0;
+        for (int i = 0; i < 2 * n - 1; ++i) {
+            int l = i / 2, r = i / 2 + i % 2;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                --l;
+                ++r;
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 马拉车算法
+     */
+    public int manacher(String s) {
+        int n = s.length();
+        StringBuffer t = new StringBuffer("$#");
+        for (int i = 0; i < n; ++i) {
+            t.append(s.charAt(i));
+            t.append('#');
+        }
+        n = t.length();
+        t.append('!');
+
+        int[] f = new int[n];
+        int iMax = 0, rMax = 0, ans = 0;
+        for (int i = 1; i < n; ++i) {
+            // 初始化 f[i]
+            f[i] = i <= rMax ? Math.min(rMax - i + 1, f[2 * iMax - i]) : 1;
+            // 中心拓展
+            while (t.charAt(i + f[i]) == t.charAt(i - f[i])) {
+                ++f[i];
+            }
+            // 动态维护 iMax 和 rMax
+            if (i + f[i] - 1 > rMax) {
+                iMax = i;
+                rMax = i + f[i] - 1;
+            }
+            // 统计答案, 当前贡献为 (f[i] - 1) / 2 上取整
+            ans += f[i] / 2;
+        }
+
+        return ans;
+    }
+}
