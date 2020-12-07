39f0f951f7b708f9c318573ccce78a8f6c770d7e
tallon <352420160@qq.com>
2020-12-07 18:13:09 +0800

    每日一题

--- leetCode/src/main/java/array/AddStrings_0415.java
+++ leetCode/src/main/java/array/AddStrings_0415.java
@@ -2,6 +2,7 @@ package array;
 
 /**
  * 字符串模拟求和
+ * https://leetcode-cn.com/problems/add-strings/
  *
  * @author tallon
  * @version v1.0.0
@@ -23,5 +24,23 @@ public class AddStrings_0415 {
         // 计算完以后的答案需要翻转过来
         ans.reverse();
         return ans.toString();
+    }
+
+    private String add(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int add = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            sb.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+        }
+        sb.reverse();
+        return sb.toString();
     }
 }
